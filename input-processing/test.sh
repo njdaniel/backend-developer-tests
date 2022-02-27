290e971bfbdc8c16c1e3543e524b38fcc1521d9a
njdaniel <nicholasjdaniel@gmail.com>
2022-02-27 11:48:19 -0500

    test 10GB file

--- input-processing/test.sh
+++ input-processing/test.sh
@@ -11,3 +11,8 @@ echo ""
 
 echo "Testing 100KB input error"
 yes testing error | head -c 100KB | go run main.go
+
+echo ""
+
+echo "Testing 10GB input error"
+yes testing error | head -c 10GB | go run main.go
