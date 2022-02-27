2798436d7f0396275b944d125d8ca426feecf014
njdaniel <nicholasjdaniel@gmail.com>
2022-02-27 10:53:27 -0500

    add script to test inputs

---
+++ input-processing/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "************************"
+echo "Starting testing input processing"
+echo "************************"
+
+echo "Testing 100KB input no error"
+yes testing noerr | head -c 100KB | go run main.go
+
+echo ""
+
+echo "Testing 100KB input error"
+yes testing error | head -c 100KB | go run main.go
